412c312ffd57351d7218dd3ffc9fcb68d3b3bce0
Oromion <caznaranl@uni.pe>
2018-01-08 23:30:12 -0500

    Initial commit

---
+++ Clase 1/exercise1_1.js
@@ -0,0 +1,17 @@
+var a = 12;
+
+if(true){
+  console.log("Verdadero");
+  for(var i = 0; i < 10; i++){
+    console.log(i + "Cadena");
+  };
+  var c,d;
+  function mifuncion(a,b){
+        return c = a, d = b;
+  };
+  mifuncion(4,5);
+  console.log("mi primer valor: "+ c + "," + "mi \segundo valor:" + d);
+}else{
+  console.log("Falso");
+};
+//console.log(a);
