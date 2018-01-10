96f0059488ed71a616a95b5b5f72c35ff01460c1
Oromion <caznaranl@uni.pe>
2018-01-09 22:57:25 -0500

    Initial commit

--- Clase 1/server/server.js
+++ Clase 1/server/server.js
@@ -4,6 +4,8 @@
 //En Angular se ve el import.
 var express = require("express");
 //var router = express.Router();
+//llamamos a mongoose
+var mongoose = require("mongoose");
 
 var app = express();
 
@@ -11,6 +13,15 @@ var app = express();
 var puerto = 3000;
 var server = app.listen(puerto, function(){
     console.log("Servidor escuchando en el puerto:" + puerto);
+});
+
+//conexion con la base de datos MongoDB
+var conection = mongoose.connect("mongodb://data1:123456@ds147167.mlab.com:47167/dbcrud", function(error){
+    if(error){
+      console.log("Error al conectarse a la base de datos");
+    }else{
+      console.log("Conectado a la base de datos");
+    }
 });
 
 var rutas = require("./rutas");
