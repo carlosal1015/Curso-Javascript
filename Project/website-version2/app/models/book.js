949da6d238b7fee5734488ff9c06f03367a9f9a5
Oromion <caznaranl@uni.pe>
2018-02-05 22:24:01 -0500

    commit

--- Project/website-version2/app/models/book.js
+++ Project/website-version2/app/models/book.js
@@ -1,3 +1,15 @@
 'use-strict';
 
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const newBook = new Schema({
+    title: String,
+    ciclo: String,
+    author: String,
+    school: String
+},{
+    collection: 'books'
+});
+
+mongoose.model('book',newBook)
