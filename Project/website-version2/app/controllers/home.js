22f68c5f104b22eb43502d6112ab0cdaf9423021
Oromion <caznaranl@uni.pe>
2018-02-05 23:50:38 -0500

    commit

--- Project/website-version2/app/controllers/home.js
+++ Project/website-version2/app/controllers/home.js
@@ -6,10 +6,12 @@ const Book = mongoose.model('Book');
 
 module.exports = (app) => {
   app.get('/', routes.getIndex);
-  app.get('/add-book', routes.getAddBook)
+  app.get('/add-book', routes.getAddBook);
+  // JSON
+  app.get('/api', routes.getApi);
+  // POST add book
+  app.post('/add-book', routes.postAddBook);
 };
-
-
 
 /*router.get('/', (req, res, next) => {
   Article.find((err, articles) => {
@@ -28,11 +30,34 @@ let routes = {
       title:"Website cec uni"
     });
   },//no va ;
+  getApi : (req, res, next) => {
+	 	Book.find()
+	 		.limit()
+	 		.sort()
+	 		.exec((err, data) => {
+	 			res.json(data)
+	 		});
+	 },
   getAddBook : (req, res, next)=>{
     res.render("add-book", {
         title:'Agregar libro'
     });
-  }
+  },
+  postAddBook : (req, res, next) => {
+     Book.find()
+       .limit()
+       .sort()
+       .exec((err, data) => {
+         var book = new book({
+           title: req.body.title,
+           author: req.body.author,
+           cicle: req.body.ciclo,
+           school: req.body.school
+         })
+       book.save();
+       res.redirect("/api");
+       })
+    }
 }
 
 // var objeto = {
